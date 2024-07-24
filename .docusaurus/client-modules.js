6ed6866a55fb89f471561251de726dff716252b2
celes <debliquy.cameron@gmail.com>
2024-07-24 15:05:08 +0200

    feat: ajout du build rly

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/home/celes/travaux/esgi/3IABD/coursGit/docusaurus-ci-exam/node_modules/infima/dist/css/default/default.css"),
+  require("/home/celes/travaux/esgi/3IABD/coursGit/docusaurus-ci-exam/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/home/celes/travaux/esgi/3IABD/coursGit/docusaurus-ci-exam/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/home/celes/travaux/esgi/3IABD/coursGit/docusaurus-ci-exam/src/css/custom.css"),
+];
